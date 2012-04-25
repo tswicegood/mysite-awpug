f2ae01b00718b86a5991081aad6438b946640496
Travis Swicegood <development@domain51.com>
2012-04-25 06:03:01 -0500

    state of models at the end of the talk

--- library/models.py
+++ library/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    page_length = models.IntegerField()
+    authors = models.ManyToManyField(Author, related_name="books")
+
+    class Meta:
+        ordering = ["page_length"]
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, **kwargs):
+        return super(Book, self).save(**kwargs)
